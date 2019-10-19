9a8fc1d6ef080ba1d15da63f6e1542d6916f0f4e
KingHarley <cweiss1995@gmail.com>
2019-10-19 21:27:15 +0800

    Updating the files

--- main.cpp
+++ main.cpp
@@ -5,14 +5,21 @@
 int main()
 {
 	auto v = read_int_vector();
+	auto sorted_v = sort(v);
 
-	std::cout << "Average: " << average(v) << "\n";
-	std::cout << "Sum: " << sum(v) << "\n";
-
-	std::cout << "Elements greater than 5: ";
-	for (auto e : filter_greater_than(v, 5))
+	std::cout << "You entered:";
+	for (auto e : sorted_v)
 		std::cout << " " << e;
 	std::cout << "\n";
+
+	for (int i = 0; i < 10; ++i)
+	{
+		if (binary_search(sorted_v, i))
+			std::cout << i << " was one of the numbers you entered!\n";
+		else
+			std::cout << i << " was not one of the numbers you entered!\n";
+	}
+		
 
 	return 0;
 }
--- vector_algos.hpp
+++ vector_algos.hpp
@@ -1,28 +1,17 @@
 
 // Check whether this header has been included already
-#ifndef CHAPTER_08_VECTOR_ALGOS_HPP
-#define CHAPTER_08_VECTOR_ALGOS_HPP    //Define the macro prevent inclusion of the header a second time.
+#ifndef CHAPTER_09_VECTOR_ALGOS_HPP
+#define CHAPTER_09_VECTOR_ALGOS_HPP    //Define the macro prevent inclusion of the header a second time.
 
 #include <vector>
-
 
 // Function that reads integer input from the user until they press Ctrl+Z. Returns a vector with the ints
 std::vector<int> read_int_vector();
 
-// Returns an int corresponding to the sum of elements in a vector 
-int sum(std::vector<int> v);
+// Function that sorts a vector
+std::vector<int> sort(std::vector<int> v);
 
-// Returns a vector containing the ints which are greater than x inside vector v
-std::vector<int> filter_greater_than(std::vector<int> v, int x);
-
-// Returns the average of the elements of a vector
-inline int average(std::vector<int> v)
-{
-	if (v.empty())
-		return 0;
-	int size = v.size();
-	return sum(v) / size;
-}
-
+// Function that checks if an int is in a vector
+bool binary_search(std::vector<int> v, int x);
 
 #endif
