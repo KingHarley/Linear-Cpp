c99ce461f5086870daf4e7fef320efc2451fb0d6
KingHarley <cweiss1995@gmail.com>
2019-10-20 00:33:01 +0800

    Finished Chapter 9

--- vector_algos.cpp
+++ vector_algos.cpp
@@ -1,7 +1,9 @@
 #include "vector_algos.hpp"
 #include <iostream>
 #include <string>
+#include <iterator>
 
+using iterator = std::vector<int>::iterator;  // This is like a typedef, wherever we see iterator we will get what is on the right (How namespaces work I think)
 
 // Function that reads integer input from the user until they press Ctrl+Z. Returns a vector with the ints
 std::vector<int> read_int_vector()
@@ -27,27 +29,72 @@ std::vector<int> read_int_vector()
 	return input;
 }
 
-// Returns an int corresponding to the sum of elements in a vector 
-int sum(std::vector<int> v)
+// Function that sorts a given vector using QuickSort!
+std::vector<int> sort(std::vector<int> v)
 {
-	int sum = 0;
+	// Given a range it will rearrange the first element and return the partition of that elements final place
+	iterator partition(iterator begin, iterator end);
+	
+	// Function that performs most of the sort work
+	void sort_impl(iterator begin, iterator end);
 
-	for (auto e : v)
-		sum += e;
+	sort_impl(v.begin(), v.end());
 
-	return sum;
+	return v;
 }
 
-// Returns a vector containing the ints which are greater than x inside vector v
-std::vector<int> filter_greater_than(std::vector<int> v, int x)
+void sort_impl(iterator begin, iterator end)
 {
-	std::vector<int> result;
+	// Need to redeclare partiton as it was declared in the scope of sort
+	iterator partition(iterator begin, iterator end);
 
-	for (auto e : v)
+	// First check if range is empty or only 1 (end and begin are just like indexes of an element, even though end doesn't correspond to an element, but a sort of imaginary element)
+	if (end - begin <= 1)
+		return;
+
+	// Rearrange the first element and obtain it's final position
+	auto pivot = partition(begin, end);
+
+	// Now let us sort the subranges as well 
+	sort_impl(begin, pivot);
+	sort_impl(pivot + 1, end);
+}
+
+iterator partition(iterator begin, iterator end)
+{
+	// The increment is weird here. pivot = old begin, while begin = old begin + 1. ie. pivot is one less than begin now.
+	auto pivot = begin++;
+
+	for (; begin != end; ++begin)
 	{
-		if (e > x)
-			result.push_back(e);
+		if (*pivot > *begin)
+		{
+			std::swap(*pivot, *begin);
+			++pivot;
+			std::swap(*pivot, *begin);
+		}
+	}
+	return pivot;
+}
+
+
+// Function that checks if given int is inside given vector
+bool binary_search(std::vector<int> v, int x)
+{
+	iterator bottom = v.begin(), top = v.end();
+
+	while (bottom != top)
+	{
+		auto mid = (bottom + (top - bottom) / 2);
+
+		if (x < *mid)
+			top = mid;
+		else if (x > *mid)
+			bottom = ++mid;   // If you don't increment mid here then we get stuck in a forever loop
+		else
+			return true;
 	}
 
-	return result;
+	return false;
 }
+
